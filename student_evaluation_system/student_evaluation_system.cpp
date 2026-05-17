524d91da3e26835995fca5f0bd4f9d63ad1e78ea
GabrielCarretts <gabrielcarretts@gmail.com>
2026-05-17 10:52:47 -0300

    refactor: remove redundant comment for color definitions

--- student_evaluation_system/student_evaluation_system.cpp
+++ student_evaluation_system/student_evaluation_system.cpp
@@ -4,7 +4,6 @@ using namespace std;
 int main()
 {
     // Colors //
-    // Colors //
     string const COLOR_BLUE = "\033[1;34m";
     string const COLOR_YELLOW = "\033[1;33m";
     string const COLOR_GREEN = "\033[1;32m";
